eb9c5e18e9d88692c66b0ac8e19ddc6f22ff813b
Sai-Eshwar-Reddy <saieshwarreddy.work@gmail.com>
2026-07-23 18:03:25 +0530

    listing test

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const listing = require('./models/listing.js')
 
 const Mongo_url = "mongodb://127.0.0.1:27017/bookmystay";
 async function main()
@@ -15,6 +16,18 @@ main()
     console.log(err);
 });
 
+app.get("/listingtest",async (req,res)=>{
+    let samplelisting = new listing({
+        Title : "Beach view villa",
+        Description : "It is a very good villa facing beach with a nice white sand beach",
+        Price : 6000,
+        Location : "Vizag,Rishikonda Beach",
+        Country : "India",
+    })
+    await samplelisting.save();
+    console.log("Savings are saved");
+    res.send("Success");
+})
 app.get("/",(req,res)=>{
     res.send("Root");
 })
