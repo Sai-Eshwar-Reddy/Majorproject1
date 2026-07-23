279ca8b3ca80502045ba7d530ea7892468ef38c3
Sai-Eshwar-Reddy <saieshwarreddy.work@gmail.com>
2026-07-23 08:09:13 +0530

    connected with monogodb

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const Mongo_url = "mongodb://127.0.0.1:27017/bookmystay";
+async function main()
+{
+    await mongoose.connect(Mongo_url);
+}
+main()
+.then(()=>{
+        console.log("Database connected successfully");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+app.get("/",(req,res)=>{
+    res.send("Root");
+})
 app.listen(3000,()=>{
     console.log("server is running");
-})
+});
